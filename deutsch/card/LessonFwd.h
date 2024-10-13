85f8f78efe5bbaaec8d636c5655ddbd7e742d6d9
Oleg Portnov <oleg.portnov@devline.ru>
2024-10-13 21:52:58 +0300

    chore:
    auxiliary structur

---
+++ deutsch/card/LessonFwd.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <QObject>
+
+namespace classbook {
+
+enum class Less : int {
+    Null = 0,
+    Less_1,
+    Less_2,
+    Less_3,
+    Less_4,
+    Less_5,
+    Less_6,
+    Less_7,
+    Less_8,
+    Less_9,
+    Less_10
+}; // Less
+
+} // ns classbook
