f4197b4e65a79f4c00acaf7ed62ba7972979ab87
Oleg Portnov <oleg.portnov@devline.ru>
2024-10-13 22:04:42 +0300

    fix:
    setting options for android

--- deutsch/main.cpp
+++ deutsch/main.cpp
@@ -6,8 +6,11 @@
 
 int main(int argc, char* argv[])
 {
-    qputenv("QT_ANDROID_NO_EXIT_CALL", "1");    // https://bugreports.qt.io/browse/QTBUG-82617
+#if defined(Q_OS_ANDROID)
+    qputenv("QT_ANDROID_NO_EXIT_CALL", "1");
     qputenv("QT_ANDROID_DISABLE_ACCESSIBILITY", "1");
+    qputenv("QT_NO_JAVA_STYLE_ITERATORS", "1");
+#endif
 
     QGuiApplication app(argc, argv);
 
